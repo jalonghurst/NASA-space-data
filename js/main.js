28a3600facdb6f529b75db7b241fd62d3188e1df
unknown <jacqui.longhurst@btinternet.com>
2022-08-04 15:01:25 +0200

    Mars Rover API added and fetching values successful

--- js/main.js
+++ js/main.js
@@ -52,3 +52,36 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=FOfTEcZgUmamN5Rc9EQwHTeOw0VtO
 
 
 // Mars Rover API 
+
+let datePicker = document.getElementById("mr-date");
+datePicker.min = "2012-08-06";
+
+document.querySelector("#mr-button").addEventListener("click", function() {
+    let date = datePicker.value;
+    console.log(date)
+    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=FOfTEcZgUmamN5Rc9EQwHTeOw0VtOULzadunDcHT`;
+    console.log(url)
+
+    fetch(url)
+    .then (res => res.json())
+    .then(data => {
+        console.log(data)
+        let image = data.photos[0].img_src;
+        let sol = data.photos[0].sol;
+        let cameraName = data.photos[0].camera.full_name;
+        let roverName = data.photos[0].rover.name;
+        let text = `The image below was taken by the ${cameraName} on the ${roverName} rover on Martian sol ${sol}.`;
+        console.log(data.status);
+        // document.querySelector('#title').innerText = data.title
+        document.querySelector("#mr-img").src = image;
+        document.querySelector("#mr-description").innerHTML = text;
+        // document.querySelector('.apod').classList.toggle('hidden');
+        // document.querySelector('.apod-video').classList.add('hidden'); 
+    })
+    .catch(err => {
+        let error = "Sorry, there are no photos on this date! Pick another.";
+        document.querySelector("#mr-description").innerHTML = error;
+        console.log(`error ${err}`)
+    });
+
+});
